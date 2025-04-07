6f0f505f83a96d2e70b5fcf633215a5c2b8e79a5
gtwglenn <gtw.glenn@gmail.com>
2025-04-07 16:00:00 -0400

    Module 7 Lab 3 commit - update files to Lab 3 directions

--- Module6/first-react-app/src/components/BitcoinRates.jsx
+++ Module6/first-react-app/src/components/BitcoinRates.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'; 
 import useBitcoinRates from '../hooks/useBitcoinRates.jsx'; 
+import { MoodContext } from './MoodContext.jsx'; 
 
 const currencies = ['USD', 'AUD', 'NZD', 'GBP', 'EUR', 'SGD'];
+
+// export default function BitcoinRates() {  ???
 
 function BitcoinRates() {
 
     const [currency, setCurrency] = useState(currencies[0]);
     const { rate, loading, error } = useBitcoinRates(currency); 
+    const { mood } = useContext(MoodContext); 
 
     const options = currencies.map((curr) => (
 
@@ -17,27 +21,46 @@ function BitcoinRates() {
 
     return (
 
-        <div className="BitcoinRates componentBox">
-            <h3>Bitcoin Exchange Rate</h3>
+        <div className="BTC_container-main">
+
+            <div className="CSS CLASS NAME FOR DISPLAY BOX">
+                <h3>Bitcoin Exchange Rate</h3>
 
 
-        <label>
-            Choose Currency: 
-            <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
-                {options}
-            </select>
-        </label>
+            <label>
+                Choose Currency: 
+                <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+                    {options}
+                </select>
+            </label>
 
 
-        {loading && <p>Loading...</p>}
-        {error && <p style={{ color: 'red' }}>Error: {error}</p>}
-        {rate !== null && !loading && !error && (
-            <p>
-                1 BTC = {rate} {currency}
-            </p>
-        )}
+            {loading && <p>Loading...</p>}
+            {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+            {rate !== null && !loading && !error && (
+                <p>
+                    1 BTC = {rate} {currency}
+                </p>
+            )}
+
+            </div>
+
+            <div className="CSS CLASS NAME FOR DISPLAY BOX">
+
+                <h3>Somewhere along the blockchain, a computer says: {mood} </h3>
+
+                <p>So, this should just display the defeault/useState value \n 
+                    or whatever the current value of openSquiqly-mood-closedSquiqly is? \n
+                    I am just confused as to where the div produced by \n 
+                    Emoji-dot-jsx would be displayed \n
+                    ie where the button to change the mood is...? </p>                
+
+            </div>
+
+
 
         </div>
+
 
     );
 
