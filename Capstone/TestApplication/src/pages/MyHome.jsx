aa57d6b97e6b10ef0402431a5fa2d0671854e219
gtwglenn <gtw.glenn@gmail.com>
2025-05-15 00:29:49 -0400

    front end work to Capstone Project

---
+++ Capstone/TestApplication/src/pages/MyHome.jsx
@@ -0,0 +1,4 @@
+import { Typography } from "@mui/material";
+export default function MyHome() {
+  return <Typography variant="h4">Welcome to My Home!</Typography>;
+}
---
+++ Capstone/TestApplication/src/pages/StoreLocator.jsx
@@ -0,0 +1,4 @@
+import { Typography } from "@mui/material";
+export default function StoreLocator() {
+  return <Typography variant="h4">Welcome to Store Locator!</Typography>;
+}
